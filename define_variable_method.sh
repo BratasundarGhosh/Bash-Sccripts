b43ed5a49d488cf71e4f41743d5f96475773b829
BratasundarGhosh <38364432+BratasundarGhosh@users.noreply.github.com>
2025-06-16 00:02:05 +0530

    Create define_variable_method.sh

---
+++ define_variable_method.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Author- Bratasundar Ghosh
+#Date- 15/06/2025
+#Description- How to define variables and call them in bash shell
+#Modify date- 15/06/2025
+
+#Define variables
+P=pwd
+W=whoami
+L=ls
+D=date
+c='cal 2025'
+echo Run variable tasks
+echo
+$P
+echo
+$W
+echo
+$L
+echo
+$D
+echo
+$c
+echo
