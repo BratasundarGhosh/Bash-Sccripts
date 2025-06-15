171f92f9fe608e449c3e896b32e8f85bd3753c23
BratasundarGhosh <38364432+BratasundarGhosh@users.noreply.github.com>
2025-06-16 00:14:52 +0530

    Create Input_Output_method.sh

---
+++ Input_Output_method.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Date- 16/06/2025
+#Decription - Input Output of data with bash shell
+#Modify Date- 16/06/2025
+echo
+echo What is your name?
+read nameconst
+echo
+echo Hello $nameconst
+echo
