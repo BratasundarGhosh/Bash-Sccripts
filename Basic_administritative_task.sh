f80532ecefe415c7d751e4948cacc1d30a3bf548
BratasundarGhosh <38364432+BratasundarGhosh@users.noreply.github.com>
2025-06-15 23:26:15 +0530

    Create Basic_administritative_task.sh

---
+++ Basic_administritative_task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Author- Bratasundar Ghosh
+#Date- 15/06/2025
+#Dscription- This script will run baic admin tasks
+#Modify Date- 15/06/2025
+echo
+pwd
+echo
+ls
+echo
+whoami
+echo
+date
+echo
+cal
+echo
+
